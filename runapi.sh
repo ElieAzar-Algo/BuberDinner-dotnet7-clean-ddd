4df1db068b54e9e08eaacb44dd03e94c7b9671aa
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2023-07-11 13:30:35 +0300

    feat(authentication): api,contract, application with dependencyInjection

--- runapi.sh
+++ runapi.sh
@@ -3,7 +3,7 @@
 # Run the dotnet command and capture the output
 # output=$(dotnet run --project ./BuberDinner.Api)
 dotnet run --project ./BuberDinner.Api
-xdg-open http://localhost:5110/swagger/index.html
+# xdg-open http://localhost:5110/swagger/index.html
 
 # # Extract the listening port from the output
 # port=$(echo "$output" | grep -oP "Now listening on: http://localhost:\K\d+")
